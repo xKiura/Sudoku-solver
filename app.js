627122d57a6ec9065389092969357aef66006a02
xKiura <63990468+xKiura@users.noreply.github.com>
2023-07-18 14:13:13 +0300

    finished filres

--- app.js
+++ app.js
@@ -2,13 +2,25 @@
 
 const puzzleBoard = document.querySelector('#puzzle');
 const solveButton = document.querySelector('#solve-button');
+const solutionDisplay = document.querySelector('#solution');
 const squares = 81;
+let submission = [];
 
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'number');
   inputElement.setAttribute('min', 1);
   inputElement.setAttribute('max', 9);
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+  ) {
+    inputElement.classList.add('odd-section');
+  }
+
   puzzleBoard.appendChild(inputElement);
 }
 
@@ -21,29 +33,41 @@ const joinValues = () => {
       submission.push('.');
     }
   });
+  console.log(submission.join(''));
 };
 
-const solve = () => {
-  joinValues();
-  const data = submissions.join('');
-  const options = {
-    method: 'POST',
-    url: 'https://solve-sudoku.p.rapidapi.com/',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'e71b7f7da8mshc86304e001dcb54p11aa82jsna31699b00074',
-      'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com',
-    },
-    data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}',
-  };
+const populateValues = (isSolvable, solution) => {
+  const inputs = document.querySelectorAll('input');
+  if (isSolvable && solution) {
+    inputs.forEach((input, i) => {
+      input.value = solution[i];
+    });
+    solutionDisplay.innerHTML = 'This is the answer';
+  } else {
+    solutionDisplay.innerHTML = 'This is not solvable';
+  }
+};
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
+const solve = async () => {
+  joinValues();
+  const data = { numbers: submission.join('') };
+
+  fetch('http://localhost:8000/solve', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      populateValues(data.solvable, data.solution);
+      submission = [];
     })
-    .catch(function (error) {
-      console.error(error);
+    .catch((error) => {
+      console.error('Error:', error);
     });
 };
 
